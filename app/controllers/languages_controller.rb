ef1ca7c970c1e804b86132e246b5349a63d41f07
Cyril Lin <bliteray@vip.qq.com>
2017-01-04 23:15:30 +0800

    HL -add language page

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,15 @@
 class LanguagesController < ApplicationController
 
   def index
+    @languages = Language.all
+
     render 'languages/index'
+  end
+
+
+  def show
+    @language = Language.find_by_id(params[:id])
+
+    render 'languages/show'
   end
 end
